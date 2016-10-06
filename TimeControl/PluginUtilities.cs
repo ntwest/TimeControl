da994d3d602859651e91756eea32d1da0697e566
Nate <natewest@gmail.com>
2016-10-06 16:06:49 -0400

    Remove LoadImageFromFile

--- TimeControl/PluginUtilities.cs
+++ TimeControl/PluginUtilities.cs
@@ -17,9 +17,11 @@ namespace TimeControl
         internal static readonly string PathApp = KSPUtil.ApplicationRootPath.Replace( "\\", "/" );
         internal static readonly string PathPlugin = System.IO.Path.GetDirectoryName( System.Reflection.Assembly.GetExecutingAssembly().Location ).Replace( "\\", "/" );
         internal static readonly string PathPluginData = string.Format( "{0}/PluginData", PathPlugin );
-        internal static readonly string PathTextures = string.Format( "{0}/Textures", PathPlugin );
-        internal static readonly string PathStockToolbarIcons = string.Format( "{0}/ToolbarIcons/StockToolbarIcons", MOD );
-        internal static readonly string PathBlizzyToolbarIcons = string.Format( "{0}/ToolbarIcons/BlizzyToolbarIcons", MOD );
+        //internal static readonly string PathTextures = string.Format( "{0}/Textures", PathPlugin );
+
+        //internal static readonly string GameDatabasePathTextures = string.Format( "{0}/Textures", MOD );
+        internal static readonly string GameDatabasePathStockToolbarIcons = string.Format( "{0}/ToolbarIcons/StockToolbarIcons", MOD );
+        internal static readonly string GameDatabasePathBlizzyToolbarIcons = string.Format( "{0}/ToolbarIcons/BlizzyToolbarIcons", MOD );
         
         internal static readonly string settingsFilePath = string.Format( "{0}/settings.cfg", PathPluginData );
 
@@ -48,67 +50,5 @@ namespace TimeControl
                                     );
             }
         }
-
-        /// <summary>
-        /// Loads a texture from the file system directly. Borrowed from Kerbal Alarm Clock (MIT Licensed)
-        /// </summary>
-        /// <param name="tex">Unity Texture to Load</param>
-        /// <param name="FileName">Image file name</param>
-        /// <param name="FolderPath">Optional folder path of image</param>
-        /// <returns></returns>
-        public static Boolean LoadImageFromFile(ref Texture2D tex, String FileName, String FolderPath = "")
-        {
-            //DebugLogFormatted("{0},{1}",FileName, FolderPath);
-            Boolean blnReturn = false;
-            try
-            {
-                if (FolderPath == "") FolderPath = PathTextures;
-
-                //File Exists check
-                if (System.IO.File.Exists( String.Format( "{0}/{1}", FolderPath, FileName ) ))
-                {
-                    try
-                    {
-                        //MonoBehaviourExtended.LogFormatted_DebugOnly( "Loading: {0}", String.Format( "{0}/{1}", FolderPath, FileName ) );
-                        tex.LoadImage( System.IO.File.ReadAllBytes( String.Format( "{0}/{1}", FolderPath, FileName ) ) );
-                        blnReturn = true;
-                    }
-                    catch (Exception ex)
-                    {
-                        //MonoBehaviourExtended.LogFormatted( "Failed to load the texture:{0} ({1})", String.Format( "{0}/{1}", FolderPath, FileName ), ex.Message );
-                    }
-                }
-                else
-                {
-                    //MonoBehaviourExtended.LogFormatted( "Cannot find texture to load:{0}", String.Format( "{0}/{1}", FolderPath, FileName ) );
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-                //MonoBehaviourExtended.LogFormatted( "Failed to load (are you missing a file):{0} ({1})", String.Format( "{0}/{1}", FolderPath, FileName ), ex.Message );
-            }
-            return blnReturn;
-        }
-
-
-        /*
-        static internal float linearInterpolate(float current, float target, float amount)
-        {
-            if (current > target && Mathf.Abs(current - target) > amount)
-            {
-                return current - amount;
-            }
-            else if (current < target && Mathf.Abs(current - target) > amount)
-            {
-                return current + amount;
-            }
-            else
-            {
-                return target;
-            }
-        }
-        */
     }
 }
